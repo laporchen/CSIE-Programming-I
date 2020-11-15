b5a33b529486b0e3c271aeefffb50248e8dbd575
Lapor <laporisbraindead@gmail.com>
2020-11-15 12:37:58 +0800

    Update mid03.c

--- MID_40947016S/mid03.c
+++ MID_40947016S/mid03.c
@@ -1,5 +1,5 @@
-#include "stdio.h"
 #include "stdint.h"
+#include "stdio.h"
 
 int main()
 {
@@ -7,23 +7,25 @@ int main()
     int32_t ex;
     int32_t open;
     printf("Please enter the Gashapon number : ");
-    if(scanf("%d",&own)!=1 || own <0)
+    if (scanf("%d", &own) != 1 || own < 0)
     {
         printf("ERROR\n");
         return 0;
     }
     printf("Please enter the exchange number : ");
-    if(scanf("%d",&ex) != 1 || ex<=0)
+    if (scanf("%d", &ex) != 1 || ex <= 0)
     {
         printf("ERROR\n");
         return 0;
     }
     open = own;
-    while(own/ex > 0)
+    while (own / ex > 0)
     {
-        open += own/ex;
+        int32_t left = own % ex;
+        open += own / ex;
         own /= ex;
+        own += left;
     }
-    printf("You can totally open %d Gashapon balls.\n",open);
+    printf("You can totally open %d Gashapon balls.\n", open);
     return 0;
 }
