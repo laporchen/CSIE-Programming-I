ce126647515fabd37cfd2868bcfd3b737f494397
Lapro Chen <laporisbraindead@gmail.com>
2020-12-23 17:13:33 +0800

    add makefile to hw6

--- HW6_40947016S/hw6.c
+++ HW6_40947016S/hw6.c
@@ -1,7 +1,19 @@
 #include "hw6.h"
 #define i32 int32_t
+#define i64 int64_t
 //hw0601
-//hw0602
+void split(i64 *input)
+{
+ 	int8_t *splitter = input;
+	int8_t *arr[8];
+	for(i32 i = 0;i<8;i++)
+	{
+		*arr[i] = *splitter;
+		splitter++;
+	} 
+}
+
+//hw0603
 void rotate(double *x,double *y,double theta)
 {
 	double d = theta;
